ce23e39c3a2c1f5ba9a40f57680155f8ae1f747b
Atul Goyal <goyalatul278@gmail.com>
2022-03-31 16:32:19 +0530

    Initial-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eCommerce copy</a>
+      </header>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>Rs {product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
