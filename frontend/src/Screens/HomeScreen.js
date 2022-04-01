f42fae470eae96b2c234135cbb6a31292b1d67c0
Atul Goyal <goyalatul278@gmail.com>
2022-04-01 15:56:07 +0530

    added-product-screen

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../Components/Product';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -42,6 +43,9 @@ function HomeScreen() {
 
   return (
     <div>
+      <Helmet>
+        <title>eCommerce</title>
+      </Helmet>
       <h1>Featured products</h1>
       <div className="products">
         {loading ? (
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,11 +1,115 @@
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Rating from '../Components/Rating';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Helmet } from 'react-helmet-async';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function ProductScreen() {
   const params = useParams(); //getting the variable slug as a param from the url
   const { slug } = params; //destructuring the params to get the slug
-  return (
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_REQUEST' });
+
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`); //we call axios.get method to fetch data from backend at this route and store it in result variable
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData(); //calling fetch function
+  }, [slug]);
+  return loading ? (
+    <div>loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <div>
-      <h1>{slug}</h1>
+      <Row>
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price : Rs{product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price</Col>
+                    <Col>Rs {product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success">In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger">Out of Stock</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
