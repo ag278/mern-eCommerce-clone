642a8767270b0c6664ddc2b96cf03149cd610cf2
Atul Goyal <goyalatul278@gmail.com>
2022-04-01 12:28:51 +0530

    added-style-and-stars-using-bootstrap

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import { useEffect, useReducer, useState } from 'react';
-import { Link } from 'react-router-dom';
 import logger from 'use-reducer-logger';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../Components/Product';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -47,23 +49,14 @@ function HomeScreen() {
         ) : error ? (
           <div>error</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              {/*link is used to remove the refreshing of the page while clicking on a link */}
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>Rs {product.price}</strong>
-                </p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                {/* {console.log(product)} */}
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
