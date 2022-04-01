edd5637a48a25b017cdffb176b56d98bed62c55f
Atul Goyal <goyalatul278@gmail.com>
2022-04-01 11:08:11 +0530

    added-use-reducer

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
+  //whenever there is a get call from frontend at this url, the backend will send the data file stored
   res.send(data.products);
-});
+});   
 
 const port = process.env.PORT || 5000;
 
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,28 +1,70 @@
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import logger from 'use-reducer-logger';
+import axios from 'axios';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  //const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_REQUEST' });
+
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('/api/products'); //we call axios.get method to fetch data from backend at this route and store it in result variable
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData(); //calling fetch function
+  }, []);
+
   return (
     <div>
       <h1>Featured products</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            {/*link is used to remove the refreshing of the page while clicking on a link */}
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>loading</div>
+        ) : error ? (
+          <div>error</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
+              {/*link is used to remove the refreshing of the page while clicking on a link */}
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>Rs {product.price}</strong>
-              </p>
-              <button>Add to Cart</button>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>Rs {product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
