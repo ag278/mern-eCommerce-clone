e63aa1c3ad3a117ba2bd324dfc9a62226fad304f
Atul Goyal <goyalatul278@gmail.com>
2022-04-02 19:55:01 +0530

    added-web-tokens

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import ProductRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,13 +13,19 @@ mongoose
   .catch((err) => console.log(err.message));
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //by using this, form data in the post request will be converted to json object inside rec.body
 app.use('/api/seed', seedRouter);
 app.use('/api/products', ProductRouter);
-
-
-
+app.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5000;
+
+//when there is error in expressasynchandler, this middleware will run and error message will be returned to the user
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.listen(port, () => {
   console.log(`Serve to port http://localhost:${port} `);
